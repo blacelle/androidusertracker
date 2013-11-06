4a8d61145f6c52d51493b9436863791ff477646c
blasd <benoit.lacelle@gmail.com>
2013-11-06 23:34:56 +0100

    Remove external parent pom

--- usertracker/src/test/java/androidusertracker/AnalyzableContext.java
+++ usertracker/src/test/java/androidusertracker/AnalyzableContext.java
@@ -1,10 +1,14 @@
 package androidusertracker;
 
 import android.content.Context;
+import android.content.ContextWrapper;
 
 import com.test.IAndroidAnalyzable;
 
-public abstract class AnalyzableContext extends Context implements IAndroidAnalyzable {
+public abstract class AnalyzableContext extends ContextWrapper implements IAndroidAnalyzable {
 
+	public AnalyzableContext(Context base) {
+		super(base);
+	}
 
 }
